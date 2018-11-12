7090eff9289f6f7def3783b3f6cfd2c2a7f4fce9
stevensm98 <megg.stevens@gmail.com>
2018-11-12 16:08:19 +0000

    edited

--- doOperator.c
+++ doOperator.c
@@ -9,6 +9,16 @@ static int op_quit(struct tokenStack *stack);
 static int op_print(struct tokenStack *stack);
 static int op_dump(struct tokenStack *stack);
 static int op_add(struct tokenStack *stack);
+static int op_diff(struct tokenStack *stack);
+static int op_product(struct tokenStack *stack);
+static int op_quotient(struct tokenStack *stack);
+static int op_GT(struct tokenStack *stack);
+static int op_LT(struct tokenStack *stack);
+static int op_GE(struct tokenStack *stack);
+static int op_LE(struct tokenStack *stack);
+static int op_EQ(struct tokenStack *stack);
+static int op_HELP();
+static int op_DROP(struct tokenStack *stack);
 
 static struct operator_struct {
   char *name;
@@ -18,6 +28,16 @@ static struct operator_struct {
   {"print", op_print},
   {"dump", op_dump},
   {"+", op_add},
+  {"-", op_diff},
+  {"*", op_product},
+  {"/", op_quotient},
+  {"GT", op_GT},
+  {"LT", op_LT},
+  {"GE", op_GE},
+  {"LE", op_LE},
+  {"EQ", op_EQ},
+  {"DROP", op_DROP},
+  {"HELP", op_HELP},
   {(char *)NULL, (int(*)(struct tokenStack *)) NULL}
 };
 
@@ -25,12 +45,22 @@ static struct operator_struct {
 /* YOU WRITE THIS */
 static int popInt(struct tokenStack *s)
 {
-  return 0;
+  return atoi(s->e[--(s->top)]->symbol);  
 }
 
 /* YOU WRITE THIS */
 static void pushInt(struct tokenStack *s, int v)
 {
+  char convert[129];
+  int i;
+  sprintf(convert, "%d", v);
+  
+  s->e[(s->top)]->kind = 3;
+  for(i = 0; convert[i] != '\0'; i++) {
+  s->e[(s->top)]->symbol[i] = convert[i];
+  }
+  s->e[(s->top)++]->symbol[i] = '\0';
+
 }
 
 int doOperator(struct tokenStack *stack, char *o) 
@@ -74,4 +104,113 @@ static int op_add(struct tokenStack *stack)
   v2 = popInt(stack);
   pushInt(stack, v1+v2);
   return(0);
+}
+
+static int op_diff(struct tokenStack *stack)
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  pushInt(stack, v2-v1);
+  return(0);
+}
+
+static int op_product(struct tokenStack *stack)
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  pushInt(stack, v1*v2);
+  return(0);
+}
+
+static int op_quotient(struct tokenStack *stack)
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  pushInt(stack, v1 / v2);
+  return(0);
+}
+
+static int op_GT(struct tokenStack *stack)
+{
+  int v1,v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  if (v1 > v2)
+    pushInt(stack, 1);
+  else
+    pushInt(stack, 0);
+  return(0);
+}
+
+static int op_LT(struct tokenStack *stack) 
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  if (v1 < v2)
+    pushInt(stack, 1);
+  else
+   pushInt(stack, 0);
+return (0);
+}
+
+static int op_GE(struct tokenStack *stack)
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  if(v1 >= v2)
+    pushInt(stack, 1);
+  else
+    pushInt(stack, 0);
+  return (0);
+}
+
+static int op_LE(struct tokenStack *stack)
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  if(v1 <= v2)
+    pushInt(stack, 1);
+  else
+    pushInt(stack, 0);
+return (0);
+}
+
+static int op_EQ(struct tokenStack *stack) 
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  if(v1 == v2)
+    pushInt(stack, 1);
+  else 
+    pushInt(stack, 0);
+return (0);
+}
+
+static int op_DROP(struct tokenStack *stack) 
+{
+  popTokenStack(stack);
+  return (0);
+}
+
+static int op_HELP() 
+{
+  printf("+ Command, add of two numbers\n");
+  printf("- Command, diff of two numbers\n");
+  printf("* Command, product of two numbers \n");
+  printf("\\ Command, quotient of two numbers \n");
+  printf("GT Command, Greater than, boolean result\n"); 
+  printf("LT Command, Less than, boolean result\n");
+  printf("GE Command, Greater than or equal, boolean result\n");
+  printf("LE Command, Less than or equal, boolean result\n");
+  printf("EQ Command, Equal command, boolean result\n");
+  printf("DROP Command, Drop last token off stack\n");
+  printf("HELP Command, print out commands\n");
+  return (0);
 }
